e561b407fe773881cff73ea63891e019a89df3fb
Artem Lobzov <psychodiss@gmail.com>
2025-07-11 19:31:29 +0300

    Initial project setup with Django, PostgreSQL, Jazzmin, static/media handling, and base app structure
     - Installed core dependencies: Django, Jazzmin, PostgreSQL adapter,
       dotenv, Pillow
    
     - Initialized Django project (conf) and created base app shop
    
     - Configured PostgreSQL database connection via .env and settings.py
    
     - Set up Docker container for local PostgreSQL instance
    
     - Applied initial DB migrations and created superuser
    
     - Registered shop and jazzmin in INSTALLED_APPS
    
     - Configured templates, static, and media settings
    
     - Added support for global and app-specific templates
    
     - Created directory structure for static/ and templates/ following Django
       conventions
    
     - Added templatetags/, forms.py, and urls.py inside the shop app
    
     - Integrated existing frontend assets into shop/static/shop/

--- conf/settings.py
+++ conf/settings.py
@@ -11,32 +11,36 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ykt)lt*h5s5)p11#a8m%9i=-dm+=2sdoz4g&&@%-$90u61119'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,17 +74,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'conf.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv('DB_NAME'),
+        'USER': getenv('DB_USER'),
+        'PASSWORD': getenv('DB_PASSWORD'),
+        'HOST': getenv('DB_ADDRESS'),
+        'PORT': getenv('DB_PORT'),
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -98,23 +113,36 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+# это просто URL-префикс
+# говорит браузеру, по какому адресу искать статику на сайте.
+STATIC_URL = '/static/'
+
+# при деплое, то, куда собирается всё для продакшена (collectstatic)
+STATIC_ROOT = BASE_DIR / 'static'
+
+# Где искать статику при разработке
+STATICFILES_DIRS = [
+    BASE_DIR / 'shop/static',
+]
+
+# то, что достаётся с базы данных
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
