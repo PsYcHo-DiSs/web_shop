4b76767e893c9374769be1a40c729afcec7273b5
Artem Lobzov <psychodiss@gmail.com>
2025-07-19 23:26:47 +0300

    feat(admin): add product thumbnail preview and product count display in admin panel
    - Implemented `get_product_image` method in `ProductAdmin` to display a
      product thumbnail in the admin list view.
    - Added HTML rendering with `mark_safe` for previewing the first
      product image if available.
    - Registered the method in `list_display` with a Russian label.
    
    - Added `get_products_count` method in `CategoryAdmin` to display
      the number of products in each category.
    - Registered the method in `list_display` with a Russian label.

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Category, Product, Gallery
+from django.utils.safestring import mark_safe
 
 
 class GalleryInline(admin.TabularInline):
@@ -11,18 +12,31 @@ class GalleryInline(admin.TabularInline):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'parent')
+    list_display = ('title', 'parent', 'get_products_count')
     prepopulated_fields = {'slug': ('title',)}
 
+    def get_products_count(self, obj):
+        if obj.products:
+            return str(len(obj.products.all()))
+        return '0'
+
+    get_products_count.short_description = 'Количество товара'
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'category', 'quantity', 'price', 'created_at', 'size', 'color')
+    list_display = ('pk', 'title', 'category', 'quantity', 'price', 'created_at', 'size', 'color', 'get_product_image')
     list_editable = ('price', 'quantity', 'size', 'color')
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('title', 'price')
     list_display_links = ('pk', 'title')
     inlines = (GalleryInline,)
+
+    def get_product_image(self, obj):
+        if obj.images.all():
+            return mark_safe(f'<img src="{obj.images.all()[0].image.url}" width="50" height="60">')
+        return "-"
+
+    get_product_image.short_description = 'Изображение товара'
 
 
 admin.site.register(Gallery)
