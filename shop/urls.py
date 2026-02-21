81c3b8b3972eabb697b32fec60e84e6910180e44
Artem Lobzov <psychodiss@gmail.com>
2026-02-21 21:07:53 +0300

    registered view function login_registration for form logics

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('category/<slug:slug>/', CategoryProductsView.as_view(), name='category_detail'),
     path('product/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('login_registration/', login_registration, name='login_registration')
 ]
