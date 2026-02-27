4143e17ff0110ac036cab86740a9ca6fe1d38491
Artem Lobzov <psychodiss@gmail.com>
2026-02-27 19:34:32 +0300

    making urls for login, logout, register features

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('category/<slug:slug>/', CategoryProductsView.as_view(), name='category_detail'),
     path('product/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
-    path('login_registration/', login_registration, name='login_registration')
+    path('login_registration/', login_registration, name='login_registration'),
+    path('login/', user_login, name='user_login'),
+    path('logout/', user_logout, name='user_logout'),
+    path('registration/', user_registration, name='user_registration'),
+
 ]
